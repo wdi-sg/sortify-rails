de52debeb21933f0deafb3862d005159e57d6b19
adna <adna@adna.com>
2018-07-19 21:49:45 +0800

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#clear the table before we seed
+Song.destroy_all
+Genre.destroy_all
+
+json = ActiveSupport::JSON.decode(File.read('db/seeds.json'))
+
+json['genre'].each do |item|
+    new_genre = Genre.new
+    new_genre.name = item['name']
+    new_genre.description = "The #{item['name']} genre"
+    new_genre.save
+end
+
+# json['song'].each do |item|
+#     new_song = Song.new
+#     new_song.name = item['name']
+#     new_song.genre_id = Genre.find_by(name: item['genre']).id
+#     new_song.save
+# end
